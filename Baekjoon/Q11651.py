78e6eb2290ced90cc6194e26ec4a46e96653b47a
kkyuwoo <ggya5@naver.com>
2023-06-01 19:44:43 +0900

    fix: 백준 11651번 나이순 정렬 - 람다 다중 조건 정렬 -> 시간 초과 - x, y 자리 바꿔서 정렬하는 방법

--- Baekjoon/Q11651.py
+++ Baekjoon/Q11651.py
@@ -2,7 +2,7 @@ n = int(input())
 dot = []
 for i in range(n):
     x, y = map(int, input().split())
-    dot.append((x, y))
-dot = sorted(dot, key=lambda x : (x[1], x[0]))
+    dot.append([y, x])
+dot.sort()
 for i in range(n):
-    print(dot[i][0], dot[i][1])
+    print(dot[i][1], dot[i][0])
