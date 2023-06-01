1ba65f33648208bc3fb315d9a3a459147a53cbac
kkyuwoo <ggya5@naver.com>
2023-06-01 20:03:41 +0900

    feat: 백준 18870번 좌표 압축 - list.index(i) 형태의 시간 복잡도 = O(N) - index[i] 형태의 시간 복잡도 = O(1)

---
+++ Baekjoon/Q18870.py
@@ -0,0 +1,14 @@
+# 시간 초과
+# n = int(input())
+# arr = list(map(int, input().split()))
+# result = sorted(list(set(arr)))
+# for i in range(n):
+#     print(result.index(arr[i]),end=' ')
+
+n = int(input())
+arr = list(map(int, input().split()))
+result = sorted(list(set(arr)))
+dic = {result[i]: i for i in range(len(result))}
+print(dic)
+for i in arr:
+    print(dic[i], end=' ')
