b5dc3b1645ffebe7dd7a079c81a24f1677a12bd8
kkyuwoo <ggya5@naver.com>
2023-06-01 21:05:34 +0900

    feat: 백준 10989번 수 정렬하기 3 - sys.stdin.readline() 사용 - 계수 정렬 : 데이터의 크기 범위가 제한된 경우, 동일한 값을 가지는 데이터가 여러개 등장할 때 효과적. 데이터가 양수일 때만 사용 가능, 값의 범위가 적당히 작을 때 좋은 성능(가장 큰 데이터와 가장 작은 데이터의 차이가 1,000,000(백만)을 넘지 않는 경우)

---
+++ Baekjoon/단계별로 풀어보기/정렬/Q10989.py
@@ -0,0 +1,20 @@
+# 메모리 초과
+# n = int(input())
+# arr = []
+# for i in range(n):
+#     arr.append(int(input()))
+# arr.sort()
+# for i in arr:
+#     print(i)
+
+import sys
+
+n = int(sys.stdin.readline())
+arr = [0] * 10001
+for _ in range(n):
+    arr[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if arr[i] != 0:
+        for _ in range(arr[i]):
+            print(i)
